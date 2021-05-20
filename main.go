833edc7256897a8f025ed51e61205a8db28d67ee
Kenje Hofileña <kenje4090@gmail.com>
2021-05-20 16:58:28 +0800

    Add manager commands

--- main.go
+++ main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"flag"
-
-	"github.com/watercompany/multiplex/worker"
-	"github.com/watercompany/multiplex/worker/client"
-)
-
-var (
-	LogName       string
-	WorkerPortNum string
-	TaskName      string
-)
-
-// for flags
-func init() {
-	flag.StringVar(&LogName, "log-name", "test", "output log name")
-	flag.StringVar(&WorkerPortNum, "worker-port", "9090", "port number a worker is listening to")
-	flag.StringVar(&TaskName, "task", "plot", "name of the task the worker will do")
-}
+import "github.com/watercompany/multiplex/cmd"
 
 func main() {
-	flag.Parse()
-
-	var wCfg worker.WorkerCfg
-	_, err := wCfg.GetWorkerCfg()
-	if err != nil {
-		panic(err)
-	}
-	var addArgs []string
-	if TaskName == "pos" {
-		addArgs, err = worker.GetPOSArgs()
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	clientCfg := client.CallWorkerConfig{
-		LogName:        LogName,
-		TaskName:       TaskName,
-		WorkerCfg:      wCfg,
-		AdditionalArgs: addArgs,
-	}
-
-	var res *worker.Result
-	client.CallWorker(clientCfg, ":"+WorkerPortNum, res)
+	cmd.Execute()
 }
